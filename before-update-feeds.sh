5361c9577719032e8bbe7de5b16363dd60e84047
Tony Guo <me@tonyguo.cn>
2021-02-19 11:04:33 +0800

    Enable luci-app-cpufreq from feeds.

--- before-update-feeds.sh
+++ before-update-feeds.sh
@@ -8,3 +8,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-app-cpufreq
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
