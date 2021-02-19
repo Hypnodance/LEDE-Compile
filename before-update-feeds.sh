a3ab3230b2d9633cd77ab3f5864e2018ac7dca31
Tony Guo <me@tonyguo.cn>
2021-02-19 11:05:47 +0800

    Add tony's packages repo to feeds.conf.default.

--- before-update-feeds.sh
+++ before-update-feeds.sh
@@ -8,6 +8,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git tony https://github.com/CuteTony/OpenWrt-Packages' feeds.conf.default
 
 # Add luci-app-cpufreq
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
