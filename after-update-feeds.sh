21f10714eb6a916138be4a2e327384ee798a150f
Tony Guo <me@tonyguo.cn>
2021-02-23 20:44:00 +0800

    Add dependences for luci-app-bypass

--- after-update-feeds.sh
+++ after-update-feeds.sh
@@ -3,5 +3,9 @@
 # Extra Config (After Update feeds)
 #
 
+#Add dependences for luci-app-bypass
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
