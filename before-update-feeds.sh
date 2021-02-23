3170d0c3b73aaea7a9f796129bb0ce8af9431c59
Tony Guo <me@tonyguo.cn>
2021-02-23 20:18:02 +0800

    Add dependences for luci-app-bypass

--- before-update-feeds.sh
+++ before-update-feeds.sh
@@ -12,3 +12,7 @@ sed -i '$a src-git tony https://github.com/CuteTony/OpenWrt-Packages' feeds.conf
 
 # Add luci-app-cpufreq
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+
+#Add dependences for luci-app-bypass
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
