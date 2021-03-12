44cad3112b51a7ad920f516a5c751239d3c8d835
Tony Guo <me@tonyguo.cn>
2021-03-13 00:21:48 +0800

    Update before-install-feeds.sh

--- before-install-feeds.sh
+++ before-install-feeds.sh
@@ -6,4 +6,4 @@
 # Dependencies for luci-app-bypass
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-
+cd package/lean && git clone https://github.com/tty228/luci-app-serverchan.git && cd ../../
