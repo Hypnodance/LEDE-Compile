409b005a863320a89cd62a98b16216f7d41a86b8
Tony Guo <me@tonyguo.cn>
2021-07-30 22:13:11 +0800

    Change feed source

--- before-update-feeds.sh
+++ before-update-feeds.sh
@@ -8,8 +8,10 @@
 
 # Add a feed source
 #sed -i '$a src-git tony https://github.com/CuteTony/OpenWrt-Packages' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 # Add luci-app-cpufreq
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
