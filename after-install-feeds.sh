c98b716e6ef6c0b7d8906031eb8f8d2cfc57a934
Tony Guo <me@tonyguo.cn>
2021-12-19 13:39:25 +0800

    Update after-install-feeds.sh

--- after-install-feeds.sh
+++ after-install-feeds.sh
@@ -9,3 +9,6 @@
 
 # iptables rule for bypass-router mode
 #sed -i 's/echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> \/etc\/firewall.user/echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> \/etc\/firewall.user\necho "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> \/etc\/firewall.user/g' package/lean/default-settings/files/zzz-default-settings
+
+# Fix OpenSSH
+sed -i '175i\  --with-sandbox=rlimit \\' feeds/packages/net/openssh/Makefile
