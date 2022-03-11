a9a3ca255ef8f63f740403bd93e227d8e5070209
Tony Guo <me@tonyguo.cn>
2022-03-12 00:09:53 +0800

    Add passswall2 feed.

--- before-update-feeds.sh
+++ before-update-feeds.sh
@@ -12,6 +12,7 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 
 # Add luci-app-cpufreq
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
